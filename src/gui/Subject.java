d76ad0d7ffe5852166482038bbfd47cc2a34c71f
nicrausaz <n.crausaz99@gmail.com>
2022-04-06 09:54:33 +0200

    Added subject

---
+++ src/gui/Subject.java
@@ -0,0 +1,26 @@
+package gui;
+
+import java.util.LinkedList;
+
+public class Subject {
+
+    LinkedList<Observer> observers;
+
+    public Subject () {
+        observers = new LinkedList<>();
+    }
+
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
+}
